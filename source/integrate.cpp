972ca1868c01cca28a85784bcdf32fdd7027c8d5
Conor Jackson <conorgjackson@gmail.com>
2019-10-12 17:39:14 +0100

    started writing wrapper for integrand

--- source/integrate.cpp
+++ source/integrate.cpp
@@ -2,6 +2,7 @@
 
 #include <complex>
 #include <utility>
+#include <stdexcept>
 
 namespace CVNI_internal {
 using std::complex;
@@ -9,15 +10,22 @@ using std::complex;
 #include <boost/math/quadrature/gauss_kronrod>
 
 template <typename Real>
-complex<Real> complex_from_py_complex(Py_complex c){
+complex<Real> complex_from_c_complex(const Py_complex& c){
     return complex<Real>(c.real,c.imag);
+}
+
+bool convertable_to_py_complex(PyObject* obj){
+    //TODO
 }
 
 template<typename Real>
 class IntegrandFunctionWrapper {
     private:
-        PyObject * callback = std::nullptr;
-        PyObject * args = std::nullptr;
+        PyObject* callback = std::nullptr;
+        PyObject* args = std::nullptr;
+        PyObject* constructArgList(Real x){
+            //TODO
+        }
     public:
         IntegrandFunctionWrapper(PyObject * func, PyObject * new_args = )
             :callback=func, args=new_args {
@@ -73,10 +81,34 @@ class IntegrandFunctionWrapper {
             // Calls the Python function callback with x as a python float
             // and args as its other arguments and reutrns the result as a
             // std::complex
-            //
-            //TODO
-            return complex(0.0,0.0);
+            
+            if(callback == std::nullptr){
+                throw std::logic_error("Attempted to call integrand function wrapper with no function defined.");
+            }
+
+            PyObject * arg_list = constructArgList(x); 
+            
+            PyObject * py_result = PyObject_CallObject(callback, arg_list);
+            
+            Py_DECREF(arg_list);
+
+            if(py_result == NULL){
+                throw PythonError("Error occured in integrand function");
+            }
+            
+            if(convertable_to_py_complex(py_result)){
+                Py_DECREF(py_result);
+                throw std::invlid_argument("The return value of the integrand function could not be converted to a complex number");
+            }
+
+            complex<Real> cpp_result = complex_from_c_complex(PyComplex_AsCComplex(py_result);
+
+            Py_DECREF(py_result);
+
+            return cpp_result;
         }
 };
+
+PyObject* integrate_internal(
 
 }
