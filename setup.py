ff70314775c3ee2d7a0c4ac3c4f5496f5ebeab78
Conor Jackson <conorgjackson@gmail.com>
2019-12-09 17:07:30 +0000

    added cpp extensions to setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
-from setuptools import setup
+from setuptools import setup,Extension
+
+src = 'source/'
 
 setup(name='Kumquat'
       version='0.1'
       author='Conor Jackson'
-      packages=['kumquat']
+      ext_package='kumquat'
+      ext_modules=[Extension('kumquat',[src+f for f in ('kumquat.cpp',
+                                            'GaussKronrod.cpp',
+                                            'IntegrandFunctionWrapper.cpp')]
+                            )]
      )
