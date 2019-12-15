7cd452e22bbf8da01bc226618ade1f83b64af648
Conor Jackson <conorgjackson@gmail.com>
2019-12-15 14:19:20 +0000

    Added metadata and test suite to setup.py

--- setup.py
+++ setup.py
@@ -7,10 +7,13 @@ boost_path = './local_boost'
 setup(name='Kumquat',
       version='0.1',
       author='Conor Jackson',
-      author_email='conrgjackson@gmail.com',
+      author_email='conorgjackson@gmail.com',
+      url='https://github.com/CGJackson/Kumquat'
       ext_modules=[Extension('kumquat',[src+f for f in ('kumquat.cpp',
                                             'GaussKronrod.cpp',
                                             'IntegrandFunctionWrapper.cpp')],
+
                                        include_dirs=[boost_path]
-                            )]
+                            )],
+      test_suite='tests.tests'
      )
