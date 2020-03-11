6f39c43feff07b42222e4f3263e58bce85edab12
Conor Jackson <conorgjackson@gmail.com>
2020-03-11 14:24:18 +0000

    Added tests on interval_infinity behaviour

--- tests/test_exp_sinh.py
+++ tests/test_exp_sinh.py
@@ -15,11 +15,25 @@ class TestExpSinh(known_interval_tests.TestSemiInfiniteIntegration):
         self.assertIsInstance(diagnostics["L1 norm"], float)
         self.assertIsInstance(diagnostics["levels"], int)
 
+    def test_accept_inteval_ininifty_by_position(self):
+        self._accept_ketword_test("interval_infinity",+1)
+        self.assertIsNotNone(self.routine_to_test(lambda x: 1j*cmath.exp(x),*self.default_range,None,None,-1))
+
     def test_accept_interval_infinity_keyword(self):
         self._accept_ketword_test("interval_infinity",+1)
-        self.assertIsNotNone(self.routine_to_test(lambda x: 1j*cmath.exp(x),*self.default_range,None,None,**{"interval_infinity":-1}))
+        self.assertIsNotNone(self.routine_to_test(lambda x: 1j*cmath.exp(x),*self.default_range,**{"interval_infinity":-1}))
 
-    def test_interval_infinity_changes_result(self):
+    def test_positional_interval_infinity_changes_result(self):
+        def asymmetric_function(x):
+            return (1.0/(x-5+1j))**2 + (1.0/(x-2j))**3
+
+        result1,*_ = self.routine_to_test(asymmetric_function, *self.default_range)
+        result2,*_ = self.routine_to_test(asymmetric_function, *self.default_range, None,None,-1)
+
+        self.assertNotAlmostEqual(result1.real,result2.real,places=self.tolerance)
+        self.assertNotAlmostEqual(result1.imag,result2.imag,places=self.tolerance)
+
+    def test_interval_infinity_keyword_changes_result(self):
         def asymmetric_function(x):
             return (1.0/(x-5+1j))**2 + (1.0/(x-2j))**3
 
@@ -42,6 +56,9 @@ class TestExpSinh(known_interval_tests.TestSemiInfiniteIntegration):
 
         self.assertAlmostEqual(pos_result.real, -neg_result.real, places=self.tolerance)
         self.assertAlmostEqual(pos_result.imag, -neg_result.imag, places=self.tolerance)
+
+    def test_ValueError_if_interval_infinity_0(self):
+        self.assertRaises(ValueError,self.routine_to_test,self.default_range,interval_infinity=0)
 
 if __name__ == '__main__':
     unittest.main()
